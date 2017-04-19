2db122543b774689e3ccdea137b938054dc7860e
natsuhonakamori <nakamori.724@gmail.com>
2017-04-19 04:26:20 +0000

    task0419

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :set_task,only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all
+    @tasks = Task.all.page(params[:page]).per(3)
   end
 
   def show
@@ -53,5 +53,5 @@ class TasksController < ApplicationController
   
   def task_params
     params.require(:task).permit(:content, :status)
-end
+  end
 end
